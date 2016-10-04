b5c013ef6e060aab68e7ba2ad2e32fa53ac2c7cf
DCSJunior <dcs.junior22@gmail.com>
2016-10-03 22:36:36 -0300

    Added pg & rails_12factor gems for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-3.1.1.min
 //= require turbolinks
 //= require_tree .
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :dstroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
+    @posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
   end
 
   def new
