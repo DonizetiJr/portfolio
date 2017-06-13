d09209a9ec9bbc2075023ffe082decdb43f0a227
DonizetiJr <dcs.junior22@gmail.com>
2017-06-13 06:10:19 -0300

    Changed fonts, colors & general styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,21 @@ class WelcomeController < ApplicationController
   def index
     @posts = Post.all.limit(4).order("created_at desc")
     @projects = Project.all.limit(4).order("created_at desc")
+    @contact = Contact.new
   end
+
+  def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+	end
 end
